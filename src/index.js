46f74b1f68d3ff3a58842bfc6a1767a7cf852362
James Mehorter <james.mehorter@pmc.com>
2020-12-02 16:17:08 -0500

    Update search bar label

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // See https://videos-hooks-ebon-five.vercel.app/
 // new in project: `$ vercel`
 // deploy an update: `$ vercel --prod`
+
+// OR
+
+// See https://vibrant-archimedes-d63c67.netlify.app
+// Installed into https://github.com/jamesmehorter/videos-hooks
+// updates on master push
